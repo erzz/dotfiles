ed2797b56e8ccc3ea446ae51c98d93ac5481befd
Sean Erswell-Liljefelt <sean.erswell-liljefelt@ingka.ikea.com>
2019-12-12 22:50:40 +0100

    custom hosts file

--- install.sh
+++ install.sh
@@ -5,3 +5,10 @@ set -e
 scripts/InstallHomebrew.sh
 scripts/LinkDotfiles.sh
 scripts/InstallOhMyZSH.sh
+
+# ###########################################################
+# /etc/hosts -- spyware/ad blocking
+# ###########################################################
+sudo cp /etc/hosts /etc/hosts.backup
+sudo cp configs/hosts /etc/hosts
+
