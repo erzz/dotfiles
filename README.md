d239469523ec2f5316848ebad5ed4317e38a4ef6
Sean Erswell-Liljefelt <sean.erswell-liljefelt@ingka.ikea.com>
2019-12-13 12:46:58 +0100

    more

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # Acknowledgment
 
-The basis of this repo is https://github.com/jponge/dotfiles 
-
-This is my own fork where I have adapted with my own tastes
+* The basis of this repo is https://github.com/jponge/dotfiles
+* Additional inspiration from https://github.com/atomantic/dotfiles
 
 ## Summary of current config
+* Bunch of apps installed via homebrew (Brewfile)
+* Zsh & Git configured via home/*
+* Whole load of OS tweaks in scripts/tweaks.sh
+* Install and config of OMZ plugins
+* Custom hosts file deployed from http://someonewhocares.org/hosts/
 
 ## Getting up and running
 
--- install.sh
+++ install.sh
@@ -5,103 +5,10 @@ set -e
 scripts/InstallHomebrew.sh
 scripts/LinkDotfiles.sh
 scripts/InstallOhMyZSH.sh
+scripts/tweaks.sh
 
 # ###########################################################
 # /etc/hosts -- spyware/ad blocking
 # ###########################################################
 sudo cp /etc/hosts /etc/hosts.backup
 sudo cp configs/hosts /etc/hosts
-# Disable remote apple events
-sudo systemsetup -f -setremoteappleevents off
-# Disable remote login
-sudo systemsetup -f -setremotelogin off
-# Disable Sudden Motion Sensor
-sudo pmset -a sms 0
-# Re-enable Desktop icons for various drive types
-defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
-defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
-# Set highlight color to green
-defaults write NSGlobalDomain AppleHighlightColor -string "0.764700 0.976500 0.568600"
-# Set sidebar icon size to medium
-defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 2
-# Expand the save and print panels by default
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
-# Disable the “Are you sure you want to open this application?” dialog
-defaults write com.apple.LaunchServices LSQuarantine -bool false
-# Reveal IP, hostname, OS, etc. when clicking clock in login window
-sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
-# Disable smart quotes as they’re annoying when typing code
-defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
-# Disable smart dashes as they’re annoying when typing code
-defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
-# Fix the scrolling direction
-defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
-# Better bluetooth sound
-defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
-# Repeating keystrokes config
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-# Screenshots
-defaults write com.apple.screencapture location -string "${HOME}/Pictures"
-defaults write com.apple.screencapture type -string "png"
-defaults write NSGlobalDomain AppleFontSmoothing -int 2
-sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
-# Finder tweaks
-defaults write com.apple.finder _FXSortFoldersFirst -bool true
-defaults write com.apple.finder NewWindowTarget -string "PfLo"
-defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
-defaults write com.apple.finder AppleShowAllFiles -bool true
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-defaults write com.apple.finder ShowStatusBar -bool true
-defaults write com.apple.finder ShowPathbar -bool true
-defaults write com.apple.finder QLEnableTextSelection -bool true
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
-defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-defaults write NSGlobalDomain com.apple.springing.enabled -bool true
-defaults write NSGlobalDomain com.apple.springing.delay -float 0
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-defaults write com.apple.frameworks.diskimages skip-verify -bool true
-defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
-defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
-defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
-defaults write com.apple.finder EmptyTrashSecurely -bool true
-defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
-# Dock and Dashboard
-defaults write com.apple.dock tilesize -int 48
-defaults write com.apple.dock minimize-to-application -bool true
-defaults write com.apple.dock show-process-indicators -bool true
-defaults write com.apple.dock hide-mirror -bool true
-# Terminals
-defaults write com.apple.terminal StringEncodings -array 4
-# Time Machine
-defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
-# Activity Monitor
-defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
-defaults write com.apple.ActivityMonitor IconType -int 5
-defaults write com.apple.ActivityMonitor ShowCategory -int 100
-defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
-defaults write com.apple.ActivityMonitor SortDirection -int 0
-defaults write com.apple.ActivityMonitor "UserColumnsPerTab v5.0" -dict \
-    '0' '( Command, CPUUsage, CPUTime, Threads, PID, UID, Ports )' \
-    '1' '( Command, ResidentSize, Threads, Ports, PID, UID,  )' \
-    '2' '( Command, PowerScore, 12HRPower, AppSleep, UID, powerAssertion )' \
-    '3' '( Command, bytesWritten, bytesRead, Architecture, PID, UID, CPUUsage )' \
-    '4' '( Command, txBytes, rxBytes, PID, UID, txPackets, rxPackets, CPUUsage )'
-defaults write com.apple.ActivityMonitor UserColumnSortPerTab -dict \
-    '0' '{ direction = 0; sort = CPUUsage; }' \
-    '1' '{ direction = 0; sort = ResidentSize; }' \
-    '2' '{ direction = 0; sort = 12HRPower; }' \
-    '3' '{ direction = 0; sort = bytesWritten; }' \
-    '4' '{ direction = 0; sort = txBytes; }'
-defaults write com.apple.ActivityMonitor UpdatePeriod -int 2
-
-# Kill affected apps
-for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
-  "Dock" "Finder" "Mail" "Messages" "Safari" "SizeUp" "SystemUIServer" \
-  "iCal" "Terminal"; do
-  killall "${app}" > /dev/null 2>&1
-done
