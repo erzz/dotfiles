36e4db2bd7e30c1d90acc543bdf721de2700e57f
Sean Erswell-Liljefelt <sean.erswell-liljefelt@ingka.ikea.com>
2019-12-13 23:35:07 +0100

    attempt to skip homebrew install

--- install.sh
+++ install.sh
@@ -2,7 +2,12 @@
 
 set -e
 
-scripts/InstallHomebrew.sh
+# Case for running as a test user on a github runner
+if id -u runner; then
+  echo "Skipping Homebrew install for Github Workflow"
+else
+  scripts/InstallHomebrew.sh
+fi
 scripts/InstallOhMyZSH.sh
 scripts/LinkDotfiles.sh
 scripts/tweaks.sh
