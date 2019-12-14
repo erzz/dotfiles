157023c6f404e34e00a92fe8cbfbd8c8e4c050c2
Sean Erswell-Liljefelt <sean.erswell-liljefelt@ingka.ikea.com>
2019-12-14 12:25:59 +0100

    fix test for config files

--- scripts/LinkDotfiles.sh
+++ scripts/LinkDotfiles.sh
@@ -2,7 +2,7 @@
 # Link all dotfiles into ~ using GNU Stow, assuming we are in ~/dotfiles
 set -ex
 for config in $(ls -A home); do
-    if [ -f $config ]; then
+    if [ -f ~/"$config" ]; then
         echo "Backing up $config to old-$config"
         mv ~/"$config" ~/old-"$config"
     fi
