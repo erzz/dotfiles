a2a67b65d4eb8e79d3f99be37edaadf6cf797986
Sean Erswell-Liljefelt <sean.erswell-liljefelt@ingka.ikea.com>
2019-12-15 21:40:29 +0100

    add dynamic refresh of hosts file from someonewhocares closes #7

--- install.sh
+++ install.sh
@@ -17,9 +17,12 @@ else
   scripts/tweaks.sh
 fi
 
-
 # ###########################################################
 # /etc/hosts -- spyware/ad blocking
 # ###########################################################
-sudo cp /etc/hosts /etc/hosts.backup
-sudo cp configs/hosts /etc/hosts
+if wget -O configs/hosts https://someonewhocares.org/hosts/hosts; then
+  sudo cp /etc/hosts /etc/hosts.backup
+  sudo cp configs/hosts /etc/hosts
+else
+  echo "Couldn't fetch the latest hosts file from https://someonewhocares.org/hosts/hosts so skipping this step in order to not screw up your computer!"
+fi
