f97b72628aa4d1139aa9112fa6259c9a8ce5d89c
Sean Erswell-Liljefelt <sean@erzz.com>
2020-08-21 15:16:34 +0200

    update to new home-brew install command

--- scripts/InstallHomebrew.sh
+++ scripts/InstallHomebrew.sh
@@ -25,6 +25,6 @@ if [ -f /usr/local/bin/brew ]; then
   already_installed
 else
   installing "Homebrew"
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
