7f25f5ec8244275f993fa224204345f6f13798ed
erzz <sean@erzz.com>
2024-03-24 17:12:57 +0100

    fix: use stow for starship

--- starship/install.sh
+++ starship/install.sh
@@ -2,15 +2,11 @@
 set -ex
 
 if ! command -v starship &>/dev/null; then
-    echo "Starship not found, installing Starship..."
-    brew install starship
+	echo "Starship not found, installing Starship..."
+	brew install starship
 else
-    echo "Starship already installed, skipping..."
+	echo "Starship already installed, skipping..."
 fi
 
 echo "Making starship pretty..."
-# create .starship and .starship/helper folder, if not exist
-mkdir -p "${HOME}/.starship"
-
-# create symlinks for starship.toml config file
-ln -sfv "${HOME}/dotfiles/starship/starship.toml" "${HOME}/.starship"
+stow -v -t "${HOME}" starship
