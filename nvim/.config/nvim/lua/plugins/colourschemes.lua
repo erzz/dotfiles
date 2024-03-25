5418aa14e7d06c359ba9f08bee3151079ddd7bd7
erzz <sean@erzz.com>
2024-03-25 20:13:14 +0100

    fix: add nightfox colorschemes

---
+++ nvim/.config/nvim/lua/plugins/colourschemes.lua
@@ -0,0 +1,11 @@
+return {
+  --- includes nightfox, dayfox, dawnfox, duskfox, nordfox, terafox, carbonfox
+  { "EdenEast/nightfox.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dayfox",
+    },
+  },
+}
