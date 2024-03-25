0bf6ce60b76aece585b35e33d8d5de82100cf156
erzz <sean@erzz.com>
2024-03-25 12:42:41 +0100

    fix: minor formatting

--- direnv/install.sh
+++ direnv/install.sh
@@ -2,10 +2,10 @@
 set -ex
 
 if ! command -v direnv version &>/dev/null; then
-    echo "direnv not found, installing..."
-    brew install direnv
+	echo "direnv not found, installing..."
+	brew install direnv
 fi
 
 echo "Configuring direnv..."
-# create symlinks for direnv.toml config file
+
 stow -v -t "${HOME}" direnv
