8779bd6c8d4f4d4eecf0edd9a27a4adac9242c8f
erzz <sean@erzz.com>
2024-03-26 20:18:31 +0100

    fix: only stow nvim config if not present

--- nvim/install.sh
+++ nvim/install.sh
@@ -2,10 +2,16 @@
 set -ex
 
 if ! command -v nvim &>/dev/null; then
-    echo "nvim not found, installing..."
-    brew install nvim
+	echo "nvim not found, installing..."
+	brew install nvim
 else
-    echo "nvim already installed, skipping..."
+	echo "nvim already installed, skipping..."
 fi
 
-stow -v -t "${HOME}" nvim
+if [ ! -d "${HOME}/.config/nvim" ]; then
+	echo "Stowing nvim config..."
+	stow -v -t "${HOME}" nvim
+else
+	echo "nvim already stowed, skipping..."
+fi
+
