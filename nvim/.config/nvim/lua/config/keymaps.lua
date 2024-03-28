2606cb3d5c75d853c5e2bad55064b651e4cfdac3
erzz <sean@erzz.com>
2024-03-29 00:23:30 +0100

    feat: add basic telescope to nvim

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,17 @@
+local keymap = vim.keymap
+
+-- Directory navigation
+keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", {noremap = true, silent = true})
+keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", {noremap = true, silent = true})
+
+-- Pane navigation
+keymap.set("n", "<C-h>", "<C-w>h", opts) --left
+keymap.set("n", "<C-l>", "<C-w>l", opts) --right
+keymap.set("n", "<C-j>", "<C-w>j", opts) --down
+keymap.set("n", "<C-k>", "<C-w>k", opts) --up
+
+-- Splits
+keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- split vertically
+keymap.set("n", "<leader>sh", ":split<CR>", opts) -- split horizontally
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts) -- toggle minimize
+
