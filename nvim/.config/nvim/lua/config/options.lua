e9698dee95680094f289562481d09f857c4e058b
erzz <sean@erzz.com>
2024-03-31 00:05:23 +0100

    fix: set colorcolumn to 100

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
+vim.opt.colorcolumn = "100"
 vim.filetype.add({
   extension = {
     mdx = "mdx",
