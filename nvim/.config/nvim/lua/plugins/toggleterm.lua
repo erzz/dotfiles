e6c0945044da91f96b0b217dc366a44a3a6403ce
erzz <sean@erzz.com>
2024-03-31 13:23:23 +0200

    feat: add vertical terminal option

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -6,7 +6,12 @@ return {
       {
         "<leader>bt",
         "<cmd>ToggleTerm size=20 direction=horizontal dir=git_dir<cr>",
-        desc = "Open a horizontal terminal",
+        desc = "Open horizontal terminal at bottom",
+      },
+      {
+        "<leader>bv",
+        "<cmd>ToggleTerm size=80 direction=vertical dir=git_dir<cr>",
+        desc = "Open vertical terminal on right",
       },
     },
     config = true,
