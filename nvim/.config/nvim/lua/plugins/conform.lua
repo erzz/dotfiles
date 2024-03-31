01fab3f878a438cd540a99afc08dddaa177a4228
erzz <sean@erzz.com>
2024-03-31 15:22:43 +0200

    fix: remove temp binding

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -2,18 +2,6 @@ return {
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
   cmd = { "ConformInfo" },
-  -- TODO: temp binding so I can test it matches with the lazy implementation
-  keys = {
-    {
-      -- Customize or remove this keymap to your liking
-      "<leader>cb",
-      function()
-        require("conform").format({ async = true, lsp_fallback = true })
-      end,
-      mode = "",
-      desc = "Format buffer with conform",
-    },
-  },
   -- Everything in opts will be passed to setup()
   opts = {
     -- Define your formatters
