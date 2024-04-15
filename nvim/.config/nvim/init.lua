fd7b93e01a9ee13adc1a53d5357dd17fd5caaa79
erzz <sean@erzz.com>
2024-04-15 10:57:39 +0200

    docs: tidy up commenting

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -11,12 +11,13 @@ vim.opt.rtp:prepend(lazypath)
 
 -- NOTE: One liners from kickstarter only, otherwise defined as custom plugins
 require('lazy').setup({
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  { 'numToStr/Comment.nvim', opts = {} }, -- "gc" to comment visual regions/lines
-
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
+  -- Custom Plugins
   { import = 'custom.plugins' },
 }, {
   ui = {
