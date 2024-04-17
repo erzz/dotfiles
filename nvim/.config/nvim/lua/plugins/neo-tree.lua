48cb98b373a9c25fafb3417e0ea5a41dbbe78046
erzz <sean@erzz.com>
2024-04-17 21:49:27 +0200

    feat: rely on neotree instead of edgy

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -3,10 +3,49 @@ return {
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
+    {
+      's1n7ax/nvim-window-picker',
+      version = '2.*',
+      config = function()
+        require('window-picker').setup {
+          filter_rules = {
+            include_current_win = false,
+            autoselect_one = true,
+            -- filter using buffer options
+            bo = {
+              -- if the file type is one of following, the window will be ignored
+              filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+              -- if the buffer type is one of following, the window will be ignored
+              buftype = { 'terminal', 'quickfix' },
+            },
+          },
+        }
+      end,
+    },
+  },
+  keys = {
+    { '<leader>ee', '<cmd>Neotree filesystem toggle left<cr>', desc = 'Toggle neotree' },
+    { '<leader>eb', '<cmd>Neotree buffers toggle left<cr>', desc = 'Toggle neotree (Buffers)' },
+    { '<leader>eg', '<cmd>Neotree git_status reveal left<cr>', desc = 'Toggle neotree (Git)' },
   },
   opts = {
+    git_status = {
+      symbols = {
+        -- Change type
+        added = '', -- or "✚", but this is redundant info if you use git_status_colors on the name
+        modified = '', -- or "", but this is redundant info if you use git_status_colors on the name
+        deleted = '✖', -- this can only be used in the git_status source
+        renamed = '󰁕', -- this can only be used in the git_status source
+        -- Status type
+        untracked = '',
+        ignored = '',
+        unstaged = '󰄱',
+        staged = '',
+        conflict = '',
+      },
+    },
     filesystem = {
       filtered_items = {
         hide_dotfiles = false,
