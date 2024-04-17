498cc713ffe81c9d0a5769dd47d64279686a3968
erzz <sean@erzz.com>
2024-04-17 22:04:44 +0200

    docs: add missing descriptors on trouble keymaps

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -12,36 +12,42 @@ return {
       function()
         require('trouble').toggle()
       end,
+      desc = '[T]oggle',
     },
     {
       '<leader>tw',
       function()
         require('trouble').toggle 'workspace_diagnostics'
       end,
+      desc = '[W]orkspace diagnostics',
     },
     {
       '<leader>td',
       function()
         require('trouble').toggle 'document_diagnostics'
       end,
+      desc = '[D]ocument diagnostics',
     },
     {
       '<leader>tq',
       function()
         require('trouble').toggle 'quickfix'
       end,
+      desc = '[Q]uickfix list',
     },
     {
       '<leader>tl',
       function()
         require('trouble').toggle 'loclist'
       end,
+      desc = '[L]ocation list',
     },
     {
       '<leader>gR',
       function()
         require('trouble').toggle 'lsp_references'
       end,
+      desc = '[R]eferences',
     },
   },
 }
