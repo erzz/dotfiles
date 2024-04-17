09ff9cc02c16214de6d6ac507a810ec95c0a5d15
erzz <sean@erzz.com>
2024-04-17 09:57:36 +0200

    fix: fine tune edgy settings and open all by default

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -43,5 +43,7 @@ require('lazy').setup({
 
 require 'config/autocommands'
 require 'config/global-keymaps'
+require('edgy').open 'left'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
