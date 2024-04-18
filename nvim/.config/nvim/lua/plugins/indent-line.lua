740f88e84f19524e3471f995685bd6ac1539fcc9
erzz <sean@erzz.com>
2024-04-18 14:02:48 +0200

    feat: add dashboard

--- nvim/.config/nvim/lua/plugins/indent-line.lua
+++ nvim/.config/nvim/lua/plugins/indent-line.lua
@@ -2,6 +2,22 @@ return {
   { -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
     main = 'ibl',
-    opts = {},
+    opts = {
+      exclude = {
+        filetypes = {
+          'help',
+          'alpha',
+          'dashboard',
+          'neo-tree',
+          'Trouble',
+          'trouble',
+          'lazy',
+          'mason',
+          'notify',
+          'toggleterm',
+          'lazyterm',
+        },
+      },
+    },
   },
 }
