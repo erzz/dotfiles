1e4e6311c251487cc141e24536e58a23499934d9
erzz <sean@erzz.com>
2025-01-07 14:19:49 +0100

    fix: switch to Ayu vim colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  'sainnhe/sonokai',
+  'ayu-theme/ayu-vim',
   priority = 1000,
   init = function()
     -- vim.g.sonokai_style = 'andromeda'
 
-    vim.cmd.colorscheme 'sonokai'
+    vim.cmd.colorscheme 'ayu'
   end,
 }
