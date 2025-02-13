04fc298b6c2c60c962290ff7600738c14935edff
erzz <sean@erzz.com>
2025-02-13 15:19:36 +0100

    fix(snacks): break out to source-specific configs for picker

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -3,7 +3,10 @@ return {
     "folke/snacks.nvim",
     opts = {
       picker = {
-        hidden = true,
+        sources = {
+          explorer = { hidden = true },
+          files = { hidden = true },
+        },
       },
     },
   },
