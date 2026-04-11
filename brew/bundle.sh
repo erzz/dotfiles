e2315cf0815584f4959baa9ea160a6a6eb51ee7d
erzz <sean@erzz.com>
2026-04-11 13:36:37 +0200

    fix: correct some minor ordering issues

--- brew/bundle.sh
+++ brew/bundle.sh
@@ -3,7 +3,9 @@ set -ex
 
 echo "Installing Homebrew packages..."
 
+DOTFILES="$(cd "$(dirname "$0")/.." && pwd)"
+
 # install packages from Brewfile
-brew bundle --file "${HOME}/dotfiles/brew/Brewfile"
+brew bundle --file "${DOTFILES}/brew/Brewfile"
 
 brew cleanup
